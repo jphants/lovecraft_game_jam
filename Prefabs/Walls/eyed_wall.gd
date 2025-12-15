63ff8948d44c86e65ef2e5bd04518ce4f0dfd02c
Jorge Núñez <94089914+jphants@users.noreply.github.com>
2025-12-15 09:11:38 -0500

    Added eye sample for walls
    * Missing remove white bg from sprites

--- Prefabs/Walls/eyed_wall.gd
+++ Prefabs/Walls/eyed_wall.gd
@@ -1,4 +1,15 @@
 extends Node3D
 
-func _ready():
-	$AnimatedSprite3D.play()
+@export var fps := 12
+@export var total_frames := 30
+
+var frame := 0
+var time := 0.0
+
+func _process(delta):
+	time += delta
+	if time >= 1.0 / fps:
+		time = 0
+		frame = (frame + 1) % total_frames
+		$MeshInstance3D2.material_override.set_shader_parameter("frame", frame)
+		
